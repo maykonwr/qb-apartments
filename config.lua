51f8bb9713cdc7c231826764c1eb12392fabde75
MaykonWr <maykonsilva60@gmail.com>
2025-03-09 01:30:43 -0300

    Apartments

---
+++ config.lua
@@ -0,0 +1,22 @@
+Apartments = {}
+Apartments.Starting = true
+Apartments.SpawnOffset = 30
+Apartments.Locations = {
+    ["apartment1"] = {
+        name = "apartment1",
+        label = "Elgin House",
+        coords = {
+            enter = vector4(-70.40, 141.80, 81.86, 214.98),
+        },
+        polyzoneBoxData = {
+            heading = 245,
+            minZ = 13.5,
+            maxZ = 16.0,
+            debug = false,
+            length = 1,
+            width = 3,
+            distance = 2.0,
+            created = false
+        }
+    },
+}
